45a2b8cd4cd163e4da21c018aeb2a5b95383f48b
KarteekAti <ati.karteek99@gmail.com>
2023-02-03 19:51:10 +0530

    clean

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Education from './components/Education/Education';
@@ -10,14 +9,14 @@ import Footer from './components/Footer/Footer';
 function App() {
 
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <Home />
       <Education />
       <Project />
       <Skills />
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
