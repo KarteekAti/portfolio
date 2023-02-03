9b1556be2a22b25e7d43f84b40b1b7fd983ce6e9
KarteekAti <ati.karteek99@gmail.com>
2023-02-03 22:05:47 +0530

    logo change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from './components/Footer/Footer';
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <Navbar />
       <Home />
       <Education />
