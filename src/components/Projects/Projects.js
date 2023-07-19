f8eb10fbc1cc6aa86347dd74370d6464b2ecdf9c
Karteek <ati.karteek99@gmail.com>
2023-07-19 16:44:04 +0530

    july - 2023

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,51 +1,77 @@
-import { UilLink, UilGithub } from '@iconscout/react-unicons'
-import Metflix from '../../Assets/Metflix.webp'
-import NSCC from '../../Assets/NSCC.png'
+import { UilLink, UilGithub } from "@iconscout/react-unicons";
+import Metflix from "../../Assets/Metflix.webp";
+import NSCC from "../../Assets/NSCC.png";
+import Talkify from "../../Assets/Talkify.png";
 
 export default function Project() {
-    const projects = [
-        {
-            title: 'Metflix Recommendation',
-            img: Metflix,
-            tech: ['Flask', 'Python', 'Javascript', 'NLTK'],
-            desc: "The Purpose of Metflix is to help people watch their favorite genre of movies. The Recommendation Model uses 'Bag of Words' and 'Cosine-Similarity' to suggest movies, this project uses TMDB's api to get the posters and cast data.",
-            github: 'https://github.com/KarteekAti/Metflix_Recommendation',
-            link: 'http://karteekati.pythonanywhere.com/'
-        },
-        {
-            title: 'Newton School Coding Club',
-            img: NSCC,
-            tech: ['NodeJs', 'Express', 'GCP', 'Firebase', 'TypeScript'],
-            desc: "This is actually a Web Application to educate students about Development and Problem-Solving using Contests, Content, Workshops, and many more interactive activities. The motive of this Mobile Application is to get notified about our contests, job opportunities, team hiring, and reading informational content.",
-            github: 'https://github.com/nsccpccoe/nscc-backend',
-            link: 'https://nsccpccoe.web.app/'
-        }
-    ]
-    return (
-        <section id='projects' className='mt-10 px-6 py-3 flex flex-col justify-center items-center'>
-            <h1 className='mb-8 font-bold text-4xl'>Projects</h1>
-            <div className='flex xs:flex-col md:flex-row gap-6'>
-                {
-                    projects.map((project, i) => (
-                        <div className="max-w-xs rounded bg-gray-100 dark:bg-[#111111] overflow-hidden shadow-md relative hover:-translate-y-2 transition-all ">
-                            <img className='w-fit' src={project.img} alt="Project Pic" />
-                            <div className="px-6 py-2">
-                                <div className="font-bold text-xl mb-2 text-purple-700 flex justify-center items-center"><a href={project.link}>{project.title}</a></div>
-                                <div className='pt-1 px-2 pb-2 flex justify-center gap-28'>
-                                    <a href={project.link}> {<UilLink className='p-1 w-16 bg-purple-700 rounded-full text-white' />}</a>
-                                    <a href={project.github}> {<UilGithub className='p-1 w-16 bg-black dark:bg-zinc-200 rounded-full text-white dark:text-black' />}</a>
-                                </div>
-                                <p className="text-gray-700 dark:text-zinc-200 text-base line-clamp-3">
-                                    {project.desc}
-                                </p>
-                            </div>
-                            <div className="px-6 pt-2 flex flex-wrap justify-center">
-                                {project.tech.map((stack, j) => <span className="inline-block bg-purple-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">{stack}</span>)}
-                            </div>
-                        </div>
-                    ))
-                }
+  const projects = [
+    {
+      title: "Talkify",
+      img: Talkify,
+      tech: ["Next.js", "GraphQL", "MongoDB", "Prisma", "Express"],
+      desc: "The purpose of the Realtime Chat Application is to facilitate seamless and interactive communication between users in real-time. The app allows users to engage in instant messaging, enabling them to have conversations with each other individually or in group chatrooms.",
+      github: "https://github.com/KarteekAti/Talkify",
+      link: "http://talkify.vercel.app/",
+    },
+    {
+      title: "Metflix Recommendation",
+      img: Metflix,
+      tech: ["Flask", "Python", "Javascript", "NLTK"],
+      desc: "The Purpose of Metflix is to help people watch their favorite genre of movies. The Recommendation Model uses 'Bag of Words' and 'Cosine-Similarity' to suggest movies, this project uses TMDB's api to get the posters and cast data.",
+      github: "https://github.com/KarteekAti/Metflix_Recommendation",
+      link: "http://karteekati.pythonanywhere.com/",
+    },
+    {
+      title: "Newton School Coding Club",
+      img: NSCC,
+      tech: ["NodeJs", "Express", "GCP", "Firebase", "TypeScript"],
+      desc: "This is actually a Web Application to educate students about Development and Problem-Solving using Contests, Content, Workshops, and many more interactive activities. The motive of this Mobile Application is to get notified about our contests, job opportunities, team hiring, and reading informational content.",
+      github: "https://github.com/nsccpccoe/nscc-backend",
+      link: "https://nsccpccoe.web.app/",
+    },
+  ];
+  return (
+    <section
+      id="projects"
+      className="mt-10 px-6 py-3 flex flex-col justify-center items-center"
+    >
+      <h1 className="mb-8 font-bold text-4xl">Projects</h1>
+      <div className="flex xs:flex-col md:flex-row gap-6">
+        {projects.map((project, i) => (
+          <div className="max-w-xs rounded bg-gray-100 dark:bg-[#111111] overflow-hidden shadow-md relative hover:-translate-y-2 transition-all ">
+            <img className="w-fit" src={project.img} alt="Project Pic" />
+            <div className="px-6 py-2">
+              <div className="font-bold text-xl mb-2 text-purple-700 flex justify-center items-center">
+                <a href={project.link}>{project.title}</a>
+              </div>
+              <div className="pt-1 px-2 pb-2 flex justify-center gap-28">
+                <a href={project.link}>
+                  {" "}
+                  {
+                    <UilLink className="p-1 w-16 bg-purple-700 rounded-full text-white" />
+                  }
+                </a>
+                <a href={project.github}>
+                  {" "}
+                  {
+                    <UilGithub className="p-1 w-16 bg-black dark:bg-zinc-200 rounded-full text-white dark:text-black" />
+                  }
+                </a>
+              </div>
+              <p className="text-gray-700 dark:text-zinc-200 text-base line-clamp-3">
+                {project.desc}
+              </p>
             </div>
-        </section>
-    )
+            <div className="px-6 pt-2 flex flex-wrap justify-center">
+              {project.tech.map((stack, j) => (
+                <span className="inline-block bg-purple-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                  {stack}
+                </span>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,67 +1,74 @@
-import ReactImage from '../../Assets/react.png'
-import HTML from '../../Assets/html.png'
-import CSS from '../../Assets/css.png'
-import Tailwind from '../../Assets/tailwind.png'
-import MUI from '../../Assets/mui.png'
-import JQuery from '../../Assets/jquery.png'
-import Node from '../../Assets/node.png'
-import Express from '../../Assets/Expressjs.png'
-import Mongo from '../../Assets/mongo.png'
-import SQL from '../../Assets/sql.png'
-import Flask from '../../Assets/flask.png'
-import JS from '../../Assets/js.png'
-import Tensorflow from '../../Assets/tensorflow.png'
-import OpenCV from '../../Assets/opencv.png'
-import Scikit from '../../Assets/scikit.png'
-import GCP from '../../Assets/gcp.png'
-import AWS from '../../Assets/aws.png'
+import ReactImage from "../../Assets/react.png";
+import HTML from "../../Assets/html.png";
+import CSS from "../../Assets/css.png";
+import Tailwind from "../../Assets/tailwind.png";
+import MUI from "../../Assets/mui.png";
+import JQuery from "../../Assets/jquery.png";
+import Node from "../../Assets/node.png";
+import Express from "../../Assets/Expressjs.png";
+import Mongo from "../../Assets/mongo.png";
+import SQL from "../../Assets/sql.png";
+import Flask from "../../Assets/flask.png";
+import JS from "../../Assets/js.png";
+import Tensorflow from "../../Assets/tensorflow.png";
+import OpenCV from "../../Assets/opencv.png";
+import Scikit from "../../Assets/scikit.png";
+import GCP from "../../Assets/gcp.png";
+import AWS from "../../Assets/aws.png";
+import GraphQL from "../../Assets/GraphQL.png";
+import NextJS from "../../Assets/NextJS.png";
 
 export default function Skills() {
-    return (
-        <section id="skills" className="mt-10 px-6 py-3 flex flex-col justify-center items-center">
-            <h1 className='mb-8 font-bold text-4xl'>Skills</h1>
-            <div className='flex flex-wrap justify-center items-center gap-6  '>
-                <div className="flex flex-col justify-center items-center md:w-1/3 shadow-md dark:bg-[#111111] bg-gray-100 text-purple-700 transition-all">
-                    <h1 className='p-4 text-2xl font-bold'>Frontend</h1>
-                    <div className='flex flex-wrap gap-6 mb-6 mx-4 justify-center'>
-                        <img src={ReactImage} className='max-h-20 '></img>
-                        <img src={HTML} className='max-h-20 '></img>
-                        <img src={CSS} className='max-h-20 '></img>
-                        <img src={Tailwind} className='max-h-20 '></img>
-                        <img src={MUI} className='max-h-20 '></img>
-                        <img src={JQuery} className='max-h-20 '></img>
-                    </div>
-                </div>
+  return (
+    <section
+      id="skills"
+      className="mt-10 px-6 py-3 flex flex-col justify-center items-center"
+    >
+      <h1 className="mb-8 font-bold text-4xl">Skills</h1>
+      <div className="flex flex-wrap justify-center items-center gap-6  ">
+        <div className="flex flex-col justify-center items-center md:w-1/3 shadow-md dark:bg-[#111111] bg-gray-100 text-purple-700 transition-all">
+          <h1 className="p-4 text-2xl font-bold">Frontend</h1>
+          <div className="flex flex-wrap gap-6 mb-6 mx-4 justify-center">
+            <img src={ReactImage} className="max-h-20 "></img>
+            <img src={NextJS} className="max-h-20 "></img>
+            <img src={HTML} className="max-h-20 "></img>
+            <img src={CSS} className="max-h-20 "></img>
+            <img src={Tailwind} className="max-h-20 "></img>
+            <img src={MUI} className="max-h-20 "></img>
+            <img src={JQuery} className="max-h-20 "></img>
+          </div>
+        </div>
 
-                <div className="mt-4 flex flex-col justify-center items-center md:w-1/2 shadow-md dark:bg-[#111111] bg-gray-100 text-purple-700 transition-all">
-                    <h1 className='p-4 text-2xl font-bold'>Backend</h1>
-                    <div className='flex flex-wrap gap-6 mb-6 mx-4 justify-center'>
-                        <img src={Node} className='max-h-16 '></img>
-                        <img src={Express} className='max-h-16 '></img>
-                        <img src={Mongo} className='max-h-12 '></img>
-                        <img src={SQL} className='max-h-16 '></img>
-                        <img src={Flask} className='max-h-16 '></img>
-                        <img src={JS} className='max-h-16 '></img>
-                    </div>
-                </div>
+        <div className="mt-4 flex flex-col justify-center items-center md:w-1/2 shadow-md dark:bg-[#111111] bg-gray-100 text-purple-700 transition-all">
+          <h1 className="p-4 text-2xl font-bold">Backend</h1>
+          <div className="flex flex-wrap gap-6 mb-6 mx-4 justify-center">
+            <img src={Node} className="max-h-16 "></img>
+            <img src={Express} className="max-h-16 "></img>
+            <img src={GraphQL} className="max-h-16 "></img>
+            <img src={Mongo} className="max-h-12 "></img>
+            <img src={SQL} className="max-h-16 "></img>
+            <img src={Flask} className="max-h-16 "></img>
+            <img src={JS} className="max-h-16 "></img>
+          </div>
+        </div>
 
-                <div className="mt-4 flex flex-col justify-center items-center md:w-1/3 shadow-md dark:bg-[#111111] bg-gray-100 text-purple-700 transition-all">
-                    <h1 className='p-4 text-2xl font-bold'>Machine Learning</h1>
-                    <div className='flex flex-wrap gap-6 mb-6 mx-4 justify-center'>
-                        <img src={Tensorflow} className='max-h-20 '></img>
-                        <img src={OpenCV} className='max-h-20 '></img>
-                        <img src={Scikit} className='max-h-20 '></img>
-                    </div>
-                </div>
+        <div className="mt-4 flex flex-col justify-center items-center md:w-1/3 shadow-md dark:bg-[#111111] bg-gray-100 text-purple-700 transition-all">
+          <h1 className="p-4 text-2xl font-bold">Machine Learning</h1>
+          <div className="flex flex-wrap gap-6 mb-6 mx-4 justify-center">
+            <img src={Tensorflow} className="max-h-20 "></img>
+            <img src={OpenCV} className="max-h-20 "></img>
+            <img src={Scikit} className="max-h-20 "></img>
+          </div>
+        </div>
 
-                <div className="mt-4 flex flex-col justify-center items-center md:w-1/2 shadow-md dark:bg-[#111111] bg-gray-100 text-purple-700 transition-all">
-                    <h1 className='p-4 text-2xl font-bold'>Cloud Technologies</h1>
-                    <div className='flex flex-wrap gap-6 mb-6 mx-4 justify-center'>
-                        <img src={GCP} className='max-h-16 '></img>
-                        <img src={AWS} className='max-h-16 '></img>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+        <div className="mt-4 flex flex-col justify-center items-center md:w-1/2 shadow-md dark:bg-[#111111] bg-gray-100 text-purple-700 transition-all">
+          <h1 className="p-4 text-2xl font-bold">Cloud Technologies</h1>
+          <div className="flex flex-wrap gap-6 mb-6 mx-4 justify-center">
+            <img src={GCP} className="max-h-16 "></img>
+            <img src={AWS} className="max-h-16 "></img>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
